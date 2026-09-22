5f0b9b5cfe7b2ab976c532eec4602976196fec5c
Shoaib Khan <shoaib7878687@gmail.com>
2026-09-22 20:01:15 +0500

    updated changes

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const express = require('express');
-const app = express()
-const port = 3000
+const express = require("express");
+const path = require("path");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const app = express();
+const port = 3000;
+
+// EJS setup
+app.set("view engine", "ejs");
+
+// Custom views folder path
+app.set("views", path.join(__dirname, "View"));
+
+// Route
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+//Instagram Activity
+
+app.get ("/:username" , (req,res) => {
+    let {username} = req.params;
+    res.render("instagram.ejs" , {username})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
